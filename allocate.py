3ca376ba56fb475692ef376da94f6eac02d23854
Archana Kumari <Archana.Kumari@cdk.com>
2018-04-07 14:23:47 +0530

    code cleanup

--- allocate.py
+++ allocate.py
@@ -13,34 +13,11 @@ def calculateColumnSum(matrix):
 
 def rearrange(debug_i, vector, col_sums, numOfDutiesLeftOnDayi, dutyCount):
     n = -1*dutyCount
-    #print("for "+ str(debug_i) + " " +str(n))
-    idxOfMoreDutiesLeft = np.argpartition(numOfDutiesLeftOnDayi, n)[n:]
-    #print(col_sums)
-    #print(idxOfMoreDutiesLeft)
-    for idx in idxOfMoreDutiesLeft:
+    idxOfMaxDutiesLeft = np.argpartition(numOfDutiesLeftOnDayi, n)[n:]
+    for idx in idxOfMaxDutiesLeft:
         vector[idx] = 1
     return vector
-##    idxOfConstraintFailed = np.where(colConstraintFailed)[0]
-##    flipBitsCount = 0
-##    idxOfZeroes = np.where([x == 0 for x in vector])[0]
-##    viableOptionforFlipping = []
-##    print("for " + str(debug_i))
-##    print(vector)
-##    print(idxOfZeroes)
-##    print(col_sums)
-##    print(colConstraintFailed)
-##    for idx in idxOfConstraintFailed:
-##        print('1')
-##        vector[idx] = 0
-##        flipBitsCount += 1
-##    for idx in idxOfZeroes:
-##        if flipBitsCount > 0 and col_sums[idx] < colConstraint:
-##            print("in")
-##            vector[idx] = 1
-##            flipBitsCount -= 1
-##    print(str(flipBitsCount)+ "while signing off") 
-    
-        
+
 
 dutyPerDay = int(input().strip())
 month, year = map(int, input().strip().split(' '))
@@ -57,11 +34,10 @@ if not correcInputCondition:
     sys.exit()
 #constraint - dutyPerDay * numOfDays = sum of employees dutycount and dutyCount < employeeCount
 
-#employeesDutyCount = sorted(employeesDutyCount,reverse=True)
 for i in range(employeeCount):
     
-    safeValue = employeeCount - dutyPerDay
-    if i < safeValue:
+    threshold = employeeCount - dutyPerDay  #we can allow only threshold values to be set randomly without hurting column count (if all are set to 0 randomly). 
+    if i < threshold:
         #a = np.random.choice([1, 0], numOfDays, p=[employeesDutyCount[i]/float(numOfDays), (numOfDays - employeesDutyCount[i])/float(numOfDays)])
         a = [1]*employeesDutyCount[i] + [0]*(numOfDays - employeesDutyCount[i])
         np.random.shuffle(a)
@@ -69,8 +45,6 @@ for i in range(employeeCount):
     else:
         col_sums = calculateColumnSum(employees)
         numOfDutiesLeftOnDayi = [(dutyPerDay - col_sum) for col_sum in col_sums]
-        #colConstraintFailed = [col_sum > dutyPerDay for col_sum in col_sums]
-        #if True in colConstraintFailed:
         a = [0] * numOfDays
         a = rearrange(i, a, col_sums, numOfDutiesLeftOnDayi, (employeesDutyCount[i]))
         employees = np.vstack([employees, a])
@@ -80,11 +54,10 @@ outFileName = 'allocation_'+str(month)+'_' + str(year)+'.csv'
 np.savetxt(outFileName, employees, delimiter=',', fmt='%s', header=','.join(headers))
 
 
-#temp:: if in doubt on count uncomment this and run code. U wud get each row count.
-#column count always satisfied..
-count = 0
-for emp in employees:
-    print(count,list(emp), sum(emp))
-    count+=1
-print(calculateColumnSum(employees))
-                        
+#temp:: if in doubt on count uncomment this and run code. U wud get each row and column count with row number.
+##count = 0
+##for emp in employees:
+##    print(count,list(emp), sum(emp))
+##    count+=1
+##print(calculateColumnSum(employees))
+                     
